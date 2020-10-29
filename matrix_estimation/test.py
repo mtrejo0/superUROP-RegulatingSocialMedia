6d3a2185bb09f4ec4d51e33b0666d787d94b70f0
Moises Trejo <moises.trejo0@gmail.com>
2020-10-29 10:52:28 -0500

    added viz

--- matrix_estimation/test.py
+++ matrix_estimation/test.py
@@ -33,13 +33,37 @@ print('User matrix: ', np.shape(U))
 # generate true rating matrix, with variance
 sigma = .1
 R = np.random.rand(m, n) * sigma + np.dot(U, V)
+print(R)
 
 # sample some values out
 mask_prob = .5
 mask = generate_mask(mask_prob, m, n)
 
 # matrix factorization, guess actual values
-R_hat = ALS(R,mask,k,.1)
+lamba = .1
+R_hat = ALS(R, mask, k, lamba)
 
+print(R_hat)
 rmse = calc_unobserved_rmse(U,V,R_hat,mask)
 print('RMSE: ', rmse)
+
+# user = int(input('Pick a user from range 0 to '+ str(m-1)+'\n'))
+
+def getTweets(data,ratings):
+  order = list(enumerate(ratings))
+  order.sort(key=lambda x: x[1])
+  # print(order)
+  tweets = [data[tweet[0]] for tweet in order]
+  return tweets
+
+recs = []
+for i in range(m):
+  rec = {}
+  rec['user'] = list(U[i])
+  rec['truth'] =  getTweets(data,R[i])
+  rec['rec'] =  getTweets(data,R[i])
+  recs.append(rec)
+
+with open('compare.json', 'w') as f:
+    json.dump(recs, f)
+
