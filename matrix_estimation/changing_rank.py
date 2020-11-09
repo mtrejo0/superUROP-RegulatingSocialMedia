8c1ce02f540fb7b60b3e1d21442a06e6350d0ce0
Serafin Garcia <sjgarcia@mit.edu>
2020-11-09 00:09:08 -0800

    Cleaned up changing_rank.py, saving before heatmap

--- matrix_estimation/changing_rank.py
+++ matrix_estimation/changing_rank.py
@@ -5,68 +5,70 @@ from scipy.stats import bernoulli
 from als import ALS
 from util import generate_mask, calc_unobserved_rmse
 
+# V = item matrix, m = # of users, k = # of user features, r is rank of user matrix
+def main_func(m, n, k, V, USER_MATRIX_RANK, MF_RANK):
+    # 1. Builds user matrix
+    M = np.random.rand(m, k)
+    U = alter_rank(M, m, k, USER_MATRIX_RANK)
 
-trials = 100
+    # 2. generate true rating matrix, with variance
+    sigma = .1
+    # R = np.random.rand(m, n) * sigma + np.dot(U, V)
+    R = np.dot(U, V)
+    R = alter_rank(R, m, n, MF_RANK)
 
-with open('twitter.json') as f:
-  data = json.load(f)
+    # 3. sample some values out
+    mask_prob = .1
+    mask = generate_mask(mask_prob, m, n)
 
-with open('social_graph.json') as f:
-  graph = json.load(f)
+    # 4. matrix factorization, guess actual values
+    lamba = .5
+    R_hat = ALS(R, mask, k, lamba)
 
-n = len(data)
-m = 25
-k = len(graph)
+    rmse = calc_unobserved_rmse(U, V, R_hat, mask)
+    return rmse
 
+# M = matrix, (m,n) = dimensions, newRank = rank to change to
+def alter_rank(M, m, n, new_rank):
+    a, b, c = np.linalg.svd(M, full_matrices=False)
+    s = np.diag(b)
+    for j in range(min(m, n)):
+        if j > new_rank:
+            s[j][j] = 0
+    return np.dot(a, np.dot(s, c))
 
-# item matrix from tweets k x n
-V = []
-for tweet in data:
-    tag = tweet['tag']
-    latent_factors = graph[tag]
-    V.append(latent_factors)
-V = np.array(V).T
+if __name__ == "__main__":
+    with open('twitter.json') as f:
+      data = json.load(f)
+    with open('social_graph.json') as f:
+      graph = json.load(f)
 
-results = []
-user_rank = []
-for r in range(m):
-    # generate random user matrix m x k
-    avg_rmse =  0
-    for t in range(trials):
-        M = np.random.rand(m, k)
-        a, b, c = np.linalg.svd(M)
-        s = np.zeros((m,k))
-        for j in range(min(m, k)):
-            if j < r:
-                s[j][j] = 0
-            else:
-                s[j][j] = b[j]
-        U = np.dot(a, np.dot(s, c.T))
+    trials = 100
+    n = len(data)
+    m = 8
+    k = len(graph)
 
-        # generate true rating matrix, with variance
-        sigma = .1
-        # R = np.random.rand(m, n) * sigma + np.dot(U, V)
-        R = np.dot(U, V)
+    # item matrix from tweets k x n
+    V = []
+    for tweet in data:
+        tag = tweet['tag']
+        latent_factors = graph[tag]
+        V.append(latent_factors)
+    V = np.array(V).T
 
-        # sample some values out
-        mask_prob = .1
-        mask = generate_mask(mask_prob, m, n)
+    results = []
+    user_rank = []
+    for r in range(min(m,k)):
+        # generate random user matrix m x k
+        avg_rmse =  0
+        for t in range(trials):
+            rmse = main_func(m, n, k, V, r, min(m,n))
+            avg_rmse += rmse
+        results.append(avg_rmse/trials)
+        user_rank.append(r + 1) # rank is 1 indexed
 
-        # matrix factorization, guess actual values
-        lamba = .5
-        R_hat = ALS(R, mask, k, lamba)
-
-        # print(R_hat)
-        rmse = calc_unobserved_rmse(U,V,R_hat,mask)
-
-        avg_rmse += rmse
-    results.append(avg_rmse/trials)
-    user_rank.append(m-r)
-
-print(results, user_rank)
-
-plt.figure()
-plt.bar(user_rank, results)
-plt.xlabel("user matrix rank")
-plt.ylabel("Average RMSE over "+str(trials)+" trials")
-plt.show()
+    plt.figure()
+    plt.bar(user_rank, results)
+    plt.xlabel("user matrix rank")
+    plt.ylabel("Average RMSE over "+str(trials)+" trials")
+    plt.show()
