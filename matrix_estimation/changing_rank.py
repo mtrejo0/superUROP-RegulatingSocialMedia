977dce6f8f9760b5685cabd4bfb2484f8f2dfe97
Serafin Garcia <sjgarcia@mit.edu>
2020-11-08 23:15:00 -0800

    modified changing_rank to handle non-square matrices

--- matrix_estimation/changing_rank.py
+++ matrix_estimation/changing_rank.py
@@ -6,34 +6,41 @@ from als import ALS
 from util import generate_mask, calc_unobserved_rmse
 
 
-n = 50
-m = 8
-k = 8
 trials = 100
 
 with open('twitter.json') as f:
   data = json.load(f)
 
+with open('social_graph.json') as f:
+  graph = json.load(f)
+
+n = len(data)
+m = 25
+k = len(graph)
+
+
 # item matrix from tweets k x n
 V = []
 for tweet in data:
-    one_hot = tweet['one_hot']
-    V.append(one_hot)
+    tag = tweet['tag']
+    latent_factors = graph[tag]
+    V.append(latent_factors)
 V = np.array(V).T
 
 results = []
 user_rank = []
-for i in range(m):
+for r in range(m):
     # generate random user matrix m x k
     avg_rmse =  0
     for t in range(trials):
-        thing1 = np.identity(m)
-        thing2 = np.random.rand(m, 1)
-        for j in range (i):
-            thing2[j][0] = 0
-        s = np.multiply(thing1, thing2)
-        M = np.random.rand(m, m)
+        M = np.random.rand(m, k)
         a, b, c = np.linalg.svd(M)
+        s = np.zeros((m,k))
+        for j in range(min(m, k)):
+            if j < r:
+                s[j][j] = 0
+            else:
+                s[j][j] = b[j]
         U = np.dot(a, np.dot(s, c.T))
 
         # generate true rating matrix, with variance
@@ -42,11 +49,11 @@ for i in range(m):
         R = np.dot(U, V)
 
         # sample some values out
-        mask_prob = .5
+        mask_prob = .1
         mask = generate_mask(mask_prob, m, n)
 
         # matrix factorization, guess actual values
-        lamba = .1
+        lamba = .5
         R_hat = ALS(R, mask, k, lamba)
 
         # print(R_hat)
@@ -54,7 +61,7 @@ for i in range(m):
 
         avg_rmse += rmse
     results.append(avg_rmse/trials)
-    user_rank.append(m-i)
+    user_rank.append(m-r)
 
 print(results, user_rank)
 
