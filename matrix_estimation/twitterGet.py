5ccb851654827215aec681a0300fbaeb7cc9aaa4
Moises Trejo <moises.trejo0@gmail.com>
2020-11-27 16:57:09 -0600

    added sports tweets

--- matrix_estimation/twitterGet.py
+++ matrix_estimation/twitterGet.py
@@ -3,16 +3,12 @@ import urllib.parse
 from util import BearerTokenAuth
 import requests
 
-# Fill these in. Generate tokens at /content/developer-twitter/en/apps. 
-
 with open('secrets.json') as f:
   secrets = json.load(f)
 
-CONSUMER_KEY = secrets['twitter']['moises']['key']
-CONSUMER_SECRET = secrets['twitter']['moises']['secret']
-query = urllib.parse.quote("has:media")
+CONSUMER_KEY = secrets['key']
+CONSUMER_SECRET = secrets['secret']
  
-
 headers = {
     "Accept-Encoding": "gzip"
 }
@@ -21,71 +17,23 @@ bearer_token = BearerTokenAuth(CONSUMER_KEY, CONSUMER_SECRET)
 
 
 
-categories = {
-    "News" : [
-        "Politics",
-        "Trump",
-        "Election",
-        "Covid",
-        "Biden",
-    ],
-    "Sports" : [
-        "Patriots",
-        "Cowboys",
-        "Football",
-        "Basketball",
-        "Baseball",
-    ],
-    "Holidays" : [
-        "Christmas",
-        "New Years",
-        "Thanksgiving",
-        "Santa",
-        "Mariahcarey",
-    ],
-    "Food" : [
-        "Pizza",
-        "Cookies",
-        "Baking",
-        "Cupcakes",
-        "Mexican",
-    ],
-    "Music" : [
-        "Ariana Grande",
-        "Drake",
-        "Violin",
-        "Piano",
-        "Justin Beiber",
-    ]
-}
+tag = 'sports'
 
-all_tags = []
-for category in categories:
-    tags = categories[category]
-    all_tags.extend(tags)
+# gets 100 tweets that are sports related with extra tags
+url = f'https://api.twitter.com/2/tweets/search/recent?query={tag}&tweet.fields=created_at&expansions=author_id&user.fields=created_at&max_results=100'
 
-totalTweets = []
-for tag in all_tags:
-    print(tag)
-    url = f'https://api.twitter.com/2/tweets/search/recent?query={tag}'
+#Make a GET request to the Labs recent search endpoint.
+response = requests.get(url, auth=bearer_token, headers = headers)
 
-    #Make a GET request to the Labs recent search endpoint.
-    response = requests.get(url, auth=bearer_token, headers = headers)
+if response.status_code != 200:
+    raise Exception(f"Request reurned an error  %s : %s" % (response.status_code, response.text))
 
-    if response.status_code != 200:
-        raise Exception(f"Request reurned an error  %s : %s" % (response.status_code, response.text))
-
-    #Display the returned Tweet JSON.
-    parsed = json.loads(response.text)
-    data = parsed['data']
-
-    pretty_print = json.dumps(data, indent=2, sort_keys=True)
-    for tweet in data[:5]:
-        tweet['tag'] = tag
-        totalTweets.append(tweet)
+#Display the returned Tweet JSON.
+parsed = json.loads(response.text)
+data = parsed['data']
 
 
-with open('twitter2.json', 'w') as f:
-    json.dump(totalTweets, f)
+with open('sports.json', 'w') as f:
+    json.dump(data, f, indent=4)
 
 print('fin')
