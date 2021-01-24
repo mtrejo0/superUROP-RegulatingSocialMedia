integrated matrix estimation, fixed masking issues

combined.py
@@ -1,5 +1,5 @@
 from topic_modeling.api import TopicModel, UserGroup
-# from matrix_estimation.api import RecommenderSystem
+from matrix_estimation.api import RecommenderSystem
 import numpy as np
 
 
@@ -27,12 +27,13 @@ if __name__ == "__main__":
                 else:
                     usergroup.updateUser(username, tweet_vec, liked=False)
         print(username + " vector: " + str(usergroup.getUser(username)))
+        print(username + " mask: " + str(usergroup.getUserMask(username)))
         username = input("Enter a new user or a type FIN to finish: ")
     R = usergroup.getRatingMatrix()
+    mask = usergroup.getMaskMatrix()
 
-    # mat_estimator = RecommenderSystem(1,1)
-
-    R_hat = R
+    mat_estimator = RecommenderSystem(R, mask, 5)
+    R_hat = mat_estimator.genRecommendedRatings()
 
     user = input("Which user to be recommended?: ")
     num_tweets_total = int(input("How big of a set of tweets to recommend from: "))
@@ -49,7 +50,7 @@ if __name__ == "__main__":
     prob_dist = prob_dist / sum(prob_dist)
 
     if is_ranked:
-        res = [x[0] for x in ranking[0:num_tweets]]
+        res = [x for x in ranking[0:num_tweets]]
     else:
         res = []
         vals = np.random.choice(len(ranking), num_tweets, p=prob_dist)

matrix_estimation/api.py
@@ -1,30 +1,36 @@
-from util import *
-from als import ALS
-from nuclear_norm import nuclear_norm_solve
+from .util import *
+from .als import ALS
+from .nuclear_norm import nuclear_norm_solve
 
 class RecommenderSystem():
 
-    def __init__(self, n, m, k = 5, sigma = .1, mask_prob = .1):
-
-        self.figure = 0
-
-        self.n = n
-        self.m = m
+    def __init__(self, R, mask, k):
+        self.R = R
         self.k = k
-
-        # item matrix from tweets k x n
-        V = np.random.rand(k, n)
-
-        # generate random user matrix m x k
-        U = np.random.rand(m, k)
-
-        # generate true rating matrix, with variance
-        self.R = np.random.rand(m, n) * sigma + np.dot(U, V)
-
-        # sample some values out
-        self.mask = generate_mask(mask_prob, m, n)
-
+        self.mask = mask
         self.R_hat = None
+
+    # def __init__(self, n, m, k = 5, sigma = .1, mask_prob = .1):
+    #
+    #     self.figure = 0
+    #
+    #     self.n = n
+    #     self.m = m
+    #     self.k = k
+    #
+    #     # item matrix from tweets k x n
+    #     V = np.random.rand(k, n)
+    #
+    #     # generate random user matrix m x k
+    #     U = np.random.rand(m, k)
+    #
+    #     # generate true rating matrix, with variance
+    #     self.R = np.random.rand(m, n) * sigma + np.dot(U, V)
+    #
+    #     # sample some values out
+    #     self.mask = generate_mask(mask_prob, m, n)
+    #
+    #     self.R_hat = None
 
 
     def getUserRankings(self, user, recommended = False):

topic_modeling/api.py
@@ -154,25 +154,29 @@ class UserGroup:
         if liked:
             self.user_vect_dict[user] += vector
         for i in range(len(vector)):
-            self.user_mask_dict[user][i][0] = 0
+            self.user_mask_dict[user][0][i] = 0
 
     def getRatingMatrix(self): # sorted by username
         names = self.userOrder
         return np.concatenate(tuple([self.getUser(name) for name in names]), axis=0)
+
+    def getMaskMatrix(self):
+        names = self.userOrder
+        return np.concatenate(tuple([self.getUserMask(name) for name in names]), axis=0)
 
 
 
 
 
 # if __name__ == "__main__":
-    # tweetbase = TweetBase("climate_tweets.csv", 10)
-    # j = tweetbase.gen_R(["a", "b", "c", "d", "e"], 9)
-    # print(j)
+#     tweetbase = TweetBase("climate_tweets.csv", 10)
+#     j = tweetbase.gen_R(["a", "b", "c", "d", "e"], 9)
+#     print(j)
+#
+#     model = TopicModel("climate_tweets.csv")
+#     model.getTopStopWords(10)
+#     usergroup = UserGroup(model.topStopwords)
 
-    # model = TopicModel("climate_tweets.csv")
-    # model.getTopStopWords(10)
-    # usergroup = UserGroup(model.topStopwords)
-    #
     # username = input("Enter a new user or a type FIN to finish: ")
     # while username != "FIN":
     #     usergroup.addUser(username)
@@ -194,6 +198,6 @@ class UserGroup:
     #     username = input("Enter a new user or a type FIN to finish: ")
     # R = usergroup.getRatingMatrix()
     # print(R)
-
+    #
     # step 1: do this
     # step 2:

topic_modeling/apiTest.py
@@ -37,7 +37,7 @@ class apiTests(unittest.TestCase):
             usergroup.updateUser("dog", vec)
         self.assertLessEqual(np.linalg.norm(usergroup.getUser("dog")), 1)
 
-    def testGenR(self):
+    def testGenRAndMask(self):
         usergroup = UserGroup(self.model.topStopwords)
         usergroup.addUser("a")
         usergroup.addUser("b")
@@ -48,10 +48,14 @@ class apiTests(unittest.TestCase):
                 vec = self.model.tweetToVector(tweet)
                 usergroup.updateUser(user, vec)
         R = usergroup.getRatingMatrix()
+        mask = usergroup.getMaskMatrix()
         for i in range(len(usergroup.userOrder)):
             x = usergroup.getUser(usergroup.userOrder[i])
             y = R[i]
             self.assertEqual(x.all(), y.all())
+            w = usergroup.getUserMask(usergroup.userOrder[i])
+            z = mask[i]
+            self.assertEqual(w.all(), z.all())
 
     def testMaskAllCoveredOneTweet(self):
         usergroup = UserGroup(self.model.topStopwords)
@@ -67,6 +71,7 @@ class apiTests(unittest.TestCase):
         usergroup.addUser("a")
         tweet = self.model.sampleTweets(1)
         vec = self.model.tweetToVector(tweet[0])
+        print(vec)
         expected = np.zeros((1, len(usergroup.topics))) + 1 - vec
         usergroup.updateUser("a", vec)
         self.assertEqual(usergroup.getUserMask("a").all(), expected.all())
@@ -77,12 +82,12 @@ class apiTests(unittest.TestCase):
         tweet = self.model.sampleTweets(10)
         vec_all = np.zeros((1, len(usergroup.topics)))
         for i in range(10):
-            vec = self.model.tweetToVector(tweet[0])
+            vec = self.model.tweetToVector(tweet[i])
             vec_all += vec
             usergroup.updateUser("a", vec)
             for i in range(len(vec)):
-                if vec_all[i][0] == 0:
-                    self.assertEqual(usergroup.getUserMask("a")[i][0], 1)
+                if vec_all[0][i] == 0:
+                    self.assertEqual(usergroup.getUserMask("a")[0][i], 1)
 
 
 