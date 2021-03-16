4e2ab3f8b6022f3b00304c426133ff655b3c0c7f
Moises Trejo <moisest@mit.edu>
2021-03-16 09:57:22 -0500

    added tweet object

--- serve.py
+++ serve.py
@@ -47,7 +47,7 @@ def add_user(username):
 
 @app.route('/user/like/<username>/<tweet_id>')
 def like_user(username, tweet_id):
-    tweet = model.retrieveTweet(int(tweet_id))
+    tweet = tweets_object.getTweet(int(tweet_id))
     tweet_vec = usergroup.tweetToVector(tweet)
     usergroup.likeTweet(username, tweet_vec)
     response = {
@@ -58,7 +58,7 @@ def like_user(username, tweet_id):
 
 @app.route('/user/show/<username>/<tweet_id>')
 def show_tweet(username, tweet_id):
-    tweet = model.retrieveTweet(int(tweet_id))
+    tweet = tweets_object.getTweet(int(tweet_id))
     tweet_vec = usergroup.tweetToVector(tweet)
     usergroup.showTweet(username, tweet_vec)
     response = {
