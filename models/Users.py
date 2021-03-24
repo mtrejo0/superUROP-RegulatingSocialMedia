122062da94660be18bdbe79b80b700e0259b9b3d
Serafin Garcia <sjgarcia@mit.edu>
2021-03-24 10:49:58 -0400

    added comments on user, added api endpoint for listing users in serve.py

--- models/Users.py
+++ models/Users.py
@@ -7,13 +7,13 @@ class Users:
         self.userOrder = []
         self.user_vect_dict = {}
         self.user_mask_dict = {}
-        self.history = {}
+        self.recommend_history = {}
 
     def getDimension(self):
         return (len(self.topics), len(self.user_vect_dict.keys()))
 
     def getUser(self, user):
-        # return sklearn.preprocessing.normalize(self.user_vect_dict[user])
+        # retrieves the preference vector for a user
         top = self.user_vect_dict[user]
         bot = np.copy(self.user_mask_dict[user])
         for i in range(len(bot[0])):
@@ -22,6 +22,7 @@ class Users:
         return np.divide(top, bot)
 
     def getUserMask(self, user):
+        # retreives the mask vector for a user
         res = np.zeros((1, len(self.topics)))
         for i in range(len(self.user_mask_dict[user][0])):
             if self.user_mask_dict[user][0][i] > 0:
@@ -29,30 +30,37 @@ class Users:
         return res
 
     def addUser(self, user):
+        # adds a user
         if user not in self.user_vect_dict.keys():
             self.user_vect_dict[user] = np.zeros((1, len(self.topics)))
             self.user_mask_dict[user] = np.zeros((1, len(self.topics)))
-            self.history[user] = []
+            self.recommend_history[user] = []
             self.userOrder.append(user)
             return
-        raise ValueError('user already exists')
+        # raise ValueError('user already exists')
+        print("User already exists!")
 
     def likeTweet(self, user, vector):
+        # updates preferences when a tweet is liked
         self.user_vect_dict[user] += vector
 
     def updateUser(self, user, vector, liked=True):
+        # enables easier sampling in command-line pipeline
         self.showTweet(user, vector)
         if liked:
             self.likeTweet(user, vector)
 
     def showTweet(self, user, vector):
+        # updates mask when a tweet is seen
         self.user_mask_dict[user] += vector
 
-    def getRatingMatrix(self): # sorted by username
+    def getRatingMatrix(self):
+        # returns the sparse rating matrix, where users are sorted by self.userOrder and topics are ordered by self.topics
         names = self.userOrder
         return np.concatenate(tuple([self.getUser(name) for name in names]), axis=0)
 
     def getMaskMatrix(self):
+        # returns the masking matrix, where users are sorted by self.userOrder and topics are ordered by self.topics
         names = self.userOrder
         return np.concatenate(tuple([self.getUserMask(name) for name in names]), axis=0)
 
