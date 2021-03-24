3c4b14238b99a1023ae183533b8bf9d80e5aa10d
Moises Trejo <moisest@mit.edu>
2021-03-24 15:46:00 -0500

    fixed recommender history

--- twitter/twitter_get.py
+++ twitter/twitter_get.py
@@ -18,7 +18,7 @@ https://developer.twitter.com/en/docs/twitter-api/tweets/search/api-reference/ge
 
 """
 
-tags = ["dog", "cat", "pet", "fish", "sandwich", "velociraptor"]
+tags = ["dog", "cat", "fish"]
 # number per tag
 n = 100
 
